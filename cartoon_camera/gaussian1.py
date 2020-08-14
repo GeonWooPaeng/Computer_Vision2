055c8cbb2a6435abf50aba06c1da2afa79a75705
GeonWooPaeng <gwpaeng@naver.com>
2020-08-14 17:43:04 +0900

    평균값 필터(블러링)

--- cartoon_camera/gaussian1.py
+++ cartoon_camera/gaussian1.py
@@ -1,9 +1,13 @@
+#블러링 효과2
+
+# 가우시안 필터
+# 가까운 픽셀은 큰 가중치를, 멀리 있는 픽셀은 작은 가중치를 사용하여 평균값 계산
 import sys
 import numpy as np
 import cv2
 
 
-src = cv2.imread('rose.bmp', cv2.IMREAD_GRAYSCALE)
+src = cv2.imread('.\cartoon_camera.\\rose.bmp', cv2.IMREAD_GRAYSCALE)
 
 dst = cv2.GaussianBlur(src, (0, 0), 3)
 dst2 = cv2.blur(src, (7, 7))
