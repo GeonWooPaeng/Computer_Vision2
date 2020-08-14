86313d7d3530e3d304b450f78717bebfd0953a65
GeonWooPaeng <gwpaeng@naver.com>
2020-08-14 17:37:57 +0900

    blurring

--- cartoon_camera/blurring1.py
+++ cartoon_camera/blurring1.py
@@ -1,17 +1,32 @@
+# 블러링 효과
+# 부드러워지는 효과
+
+# 평균값 필터(Mean filter)
+# 영상의 특정 좌표 값을 주변 픽셀 값들의 산술 평균으로 설정
+# 픽셀 간의 그레이스케일 값의 급격한 변화가 줄어들어 부드러워지고, 영상에 있는 잡음 영향이 사라지는 효과
+# 마스크가 늘어나면 연산량이 크게 증가 한다.
+
 import sys
 import numpy as np
 import cv2
 
 
-src = cv2.imread('rose.bmp', cv2.IMREAD_GRAYSCALE)
+src = cv2.imread('.\cartoon_camera.\\rose.bmp', cv2.IMREAD_GRAYSCALE)
 
 if src is None:
     print('Image load failed!')
     sys.exit()
 
+kernel = np.array([[1/9, 1/9, 1/9],
+                   [1/9, 1/9, 1/9],
+                   [1/9, 1/9, 1/9]], dtype = np.float32)
 #kernel = np.ones((3, 3), dtype=np.float64) / 9.
-#dst = cv2.filter2D(src, -1, kernel)
-dst = cv2.blur(src, (3, 3))
+
+
+dst = cv2.filter2D(src, -1, kernel) # 필터링 함수
+
+# 3 X 3 짜리를 블러링하는 함수
+# dst = cv2.blur(src, (3, 3)) 
 
 cv2.imshow('src', src)
 cv2.imshow('dst', dst)
--- cartoon_camera/blurring2.py
+++ cartoon_camera/blurring2.py
@@ -2,7 +2,7 @@ import sys
 import numpy as np
 import cv2
 
-src = cv2.imread('rose.bmp', cv2.IMREAD_GRAYSCALE)
+src = cv2.imread('.\cartoon_camera.\\rose.bmp', cv2.IMREAD_GRAYSCALE)
 
 if src is None:
     print('Image load failed!')
@@ -10,12 +10,12 @@ if src is None:
 
 cv2.imshow('src', src)
 
-for ksize in (3, 5, 7):
+for ksize in (3, 5, 7): # 3x3, 5x5, 7x7를 이용해서 블러링을 한다
     dst = cv2.blur(src, (ksize, ksize))
 
     desc = 'Mean: {}x{}'.format(ksize, ksize)
     cv2.putText(dst, desc, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
-                1.0, 255, 1, cv2.LINE_AA)
+                1.0, 255, 1, cv2.LINE_AA) #사진에 표현을 해주는 것
 
     cv2.imshow('dst', dst)
     cv2.waitKey()
