28108f98df7eb3a27ce31d3f0262178e80d12296
GeonWooPaeng <gwpaeng@naver.com>
2020-08-15 13:32:51 +0900

    언샤프 마스크 필터링(샤프닝)

--- cartoon_camera/sharpening1.py
+++ cartoon_camera/sharpening1.py
@@ -1,15 +1,27 @@
+# 샤프닝
+# 영상을 날카롭게 만들겠다
+
+#방법1 
+# 언샤프 마스크 필터링 
+# 부드러워진 영상을 이용하여 날카로운 영상을 생성
+
+#그레이스케일 샤프닝하기
+
 import sys
 import numpy as np
 import cv2
 
-src = cv2.imread('rose.bmp', cv2.IMREAD_GRAYSCALE)
+src = cv2.imread('.\cartoon_camera.\\rose.bmp', cv2.IMREAD_GRAYSCALE)
 
 if src is None:
     print('Image load failed!')
     sys.exit()
 
-blr = cv2.GaussianBlur(src, (0, 0), 2)
-dst = np.clip(2.0*src - blr, 0, 255).astype(np.uint8)
+blr = cv2.GaussianBlur(src, (0, 0), 2) #블러링 만들기(시그마: 2)
+
+#샤프닝 만들기
+dst = np.clip(2.0*src - blr, 0, 255).astype(np.uint8) 
+# dst = cv2.addWeighted(src, 2, blr, -1, 0) #src의 2배에 blr을 빼준 것
 
 cv2.imshow('src', src)
 cv2.imshow('dst', dst)
--- cartoon_camera/sharpening2.py
+++ cartoon_camera/sharpening2.py
@@ -1,8 +1,10 @@
+#컬러 영상을 샤프닝 하기 
+
 import sys
 import numpy as np
 import cv2
 
-src = cv2.imread('rose.bmp')
+src = cv2.imread('.\cartoon_camera.\\rose.bmp')
 
 if src is None:
     print('Image load failed!')
@@ -10,7 +12,8 @@ if src is None:
 
 src_ycrcb = cv2.cvtColor(src, cv2.COLOR_BGR2YCrCb)
 
-src_f = src_ycrcb[:, :, 0].astype(np.float32)
+#float32를 한 이유: gaussianblur는 입력과 출력이 같으며 uint8로 하면 소수점이 다 잘립니다. 그러면 정교한 영상을 보여주지 못하기 때문에 float32를 사용한다 
+src_f = src_ycrcb[:, :, 0].astype(np.float32) 
 blr = cv2.GaussianBlur(src_f, (0, 0), 2.0)
 src_ycrcb[:, :, 0] = np.clip(2. * src_f - blr, 0, 255).astype(np.uint8)
 
