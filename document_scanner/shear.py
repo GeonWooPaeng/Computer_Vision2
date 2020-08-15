8b520908454dc7f71be289fdd51fb0fae2124362
GeonWooPaeng <gwpaeng@naver.com>
2020-08-16 00:46:09 +0900

    전단 변환(기하학적 변환)

--- document_scanner/shear.py
+++ document_scanner/shear.py
@@ -1,21 +1,25 @@
+# 전단 변환
+# 층 밀림 변환, x축과 y축 방향에 대해 따로 정의
+
 import sys
 import numpy as np
 import cv2
 
 
-src = cv2.imread('tekapo.bmp')
+src = cv2.imread('.\document_scanner.\\tekapo.bmp')
 
 if src is None:
     print('Image load failed!')
     sys.exit()
 
 aff = np.array([[1, 0.5, 0],
-                [0, 1, 0]], dtype=np.float32)
+                [0, 1, 0]], dtype=np.float32) # x' = x+0.5y, y' = y 
 
 h, w = src.shape[:2]
-dst = cv2.warpAffine(src, aff, (w + int(h * 0.5), h))
+dst = cv2.warpAffine(src, aff, (w + int(h * 0.5), h)) #밀려 나가는 것까지 보여주게 한 것
 
 cv2.imshow('src', src)
 cv2.imshow('dst', dst)
+
 cv2.waitKey()
 cv2.destroyAllWindows()
