ce7a9d70c80d74a7d89171615d5594d74aa5ae2a
GeonWooPaeng <gwpaeng@naver.com>
2020-08-16 17:46:38 +0900

    영상의 회전

--- document_scanner/rotation1.py
+++ document_scanner/rotation1.py
@@ -1,20 +1,24 @@
+# 회전 변환 
+# 영상을 특정 각도 만큼 회전시키는 것(반시계 방향)
+# x' = cos@*x + sin@*y
+# y' = -sin@*x + cos@*y
+
 import sys
 import math
 import numpy as np
 import cv2
 
-
-src = cv2.imread('tekapo.bmp')
+src = cv2.imread('.\\document_scanner.\\tekapo.bmp')
 
 if src is None:
     print('Image load failed!')
     sys.exit()
 
-rad = 20 * math.pi / 180
+rad = 20 * math.pi / 180 #20 -> 반시계방향으로 20도
 aff = np.array([[math.cos(rad), math.sin(rad), 0],
                 [-math.sin(rad), math.cos(rad), 0]], dtype=np.float32)
 
-dst = cv2.warpAffine(src, aff, (0, 0))
+dst = cv2.warpAffine(src, aff, (0, 0)) #영상의 모든 좌표를 변환한다.
 
 cv2.imshow('src', src)
 cv2.imshow('dst', dst)
--- document_scanner/rotation2.py
+++ document_scanner/rotation2.py
@@ -3,13 +3,20 @@ import numpy as np
 import cv2
 
 
-src = cv2.imread('tekapo.bmp')
+src = cv2.imread('.\\document_scanner.\\tekapo.bmp')
 
 if src is None:
     print('Image load failed!')
     sys.exit()
 
 cp = (src.shape[1] / 2, src.shape[0] / 2)
+
+# 특정 점을 중심으로 회전할 때 사용하는 함수
+# cv2.getRotationMatrix2D(center, angle, scale)
+# center: 회전 중심
+# angle: 양수 - 반시계, 음수 - 시계
+# scale: 확대 비율
+# 출력: 2x3 어파인 변환 행렬, 실수형
 rot = cv2.getRotationMatrix2D(cp, 20, 0.5)
 
 dst = cv2.warpAffine(src, rot, (0, 0))
